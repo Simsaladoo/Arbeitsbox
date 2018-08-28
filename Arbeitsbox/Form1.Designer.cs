fb925500b21f755b93e4a9ca1418feaf5b437266
Simsaladoo <simsaladoo@hotmail.com>
2018-08-28 11:41:54 -0700

    Startup controls added for foldermaker
    working, but only creates a single folder until we finish setting up the loop

--- Arbeitsbox/Form1.Designer.cs
+++ Arbeitsbox/Form1.Designer.cs
@@ -28,9 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.minlabel = new System.Windows.Forms.Label();
+            this.maxlabel = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -42,21 +57,22 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(359, 58);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Make Folders";
+            this.button1.Text = "Choose Folders";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.SystemColors.Info;
-            this.textBox1.Location = new System.Drawing.Point(-1, -1);
+            this.textBox1.Location = new System.Drawing.Point(1, 1);
             this.textBox1.Margin = new System.Windows.Forms.Padding(0);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(361, 19);
+            this.textBox1.Size = new System.Drawing.Size(358, 16);
             this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Z:\\";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button2
@@ -72,17 +88,189 @@
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.maxlabel);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.minlabel);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, 18);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(359, 547);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(23, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sequence";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(23, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Starting Number";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(0, 489);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(359, 58);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Make Folders";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(23, 322);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ending Number";
+            // 
+            // minlabel
+            // 
+            this.minlabel.AutoSize = true;
+            this.minlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minlabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.minlabel.Location = new System.Drawing.Point(247, 237);
+            this.minlabel.Name = "minlabel";
+            this.minlabel.Size = new System.Drawing.Size(50, 24);
+            this.minlabel.TabIndex = 6;
+            this.minlabel.Text = "0000";
+            // 
+            // maxlabel
+            // 
+            this.maxlabel.AutoSize = true;
+            this.maxlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maxlabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.maxlabel.Location = new System.Drawing.Point(247, 322);
+            this.maxlabel.Name = "maxlabel";
+            this.maxlabel.Size = new System.Drawing.Size(50, 24);
+            this.maxlabel.TabIndex = 7;
+            this.maxlabel.Text = "0010";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.Location = new System.Drawing.Point(224, 164);
+            this.textBox2.MaxLength = 3;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(97, 22);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "AA";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(208, 312);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(36, 44);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "<";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(300, 312);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(36, 44);
+            this.button5.TabIndex = 10;
+            this.button5.Text = ">";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(300, 226);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(36, 44);
+            this.button6.TabIndex = 12;
+            this.button6.Text = ">";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(208, 226);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(36, 44);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "<";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(208, 147);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(128, 56);
+            this.panel2.TabIndex = 13;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.ClientSize = new System.Drawing.Size(359, 565);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(375, 600);
+            this.MinimumSize = new System.Drawing.Size(375, 400);
             this.Name = "Form1";
             this.Text = "Arbeitsbox";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,6 +281,19 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label maxlabel;
+        private System.Windows.Forms.Label minlabel;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
--- Arbeitsbox/Form1.cs
+++ Arbeitsbox/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,42 +10,217 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.WindowsAPICodePack.Dialogs;
 
+
 namespace Arbeitsbox
 {
     public partial class Form1 : Form
     {
+        // global variables 
+        public string dummyFileName;
+        public string savePath;
+        public bool haschosen;
+        public int startingnumber;
+        public int endingnumber;
+        public string sequence;
+        public string wholepath;
+
+
         public Form1()
         {
             InitializeComponent();
             /* Beginning of record */
-
-
-
-
-
-
-
-
-
-
+            haschosen = false;
+            dummyFileName = "Choose a folder";
+            textBox1.Text = "Choose a folder";
+            savePath = @"C:\Users\dmiller\Desktop";
+            panel1.Visible = false;
+            startingnumber = 0000;
+            endingnumber = 0010;
+            sequence = "ASD";
+            minlabel.Text = startingnumber.ToString();
+            maxlabel.Text = endingnumber.ToString();
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             //choose directory
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.FileName = dummyFileName;
+
+            if (sf.ShowDialog() == DialogResult.OK)
+            {
+                // Now here's our save folder
+                string savePath = Path.GetDirectoryName(sf.FileName);
+                Console.WriteLine(savePath + " " + haschosen);
+                haschosen = true;
+                textBox1.Text = savePath;
+
+            }
         }
+        
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (haschosen == true)
+            {
+                // show panel for choosing folder numbers
+                panel1.Visible = true;
+                wholepath = (savePath + @"\" + sequence + startingnumber);
+                //now update the path to show what we're working with
+                textBox1.Text = (wholepath);
 
+            }
+
+            else
+            {
+                textBox1.Text = "You must first choose a path!";
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            //make folders
+            //nothing
         }
 
 
+
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //make folders
+            //append the sequence name first, then use a loop while to create folders with incrementing #s until !< endingnumber
+
+            var folder = Path.Combine(savePath, "folder");
+            Directory.CreateDirectory(wholepath);
+
+        }
+
+
+
+
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //use this as prefix for folder name
+
+            sequence = textBox2.Text;
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // lower start number
+            if (startingnumber < 0000)
+            {
+                //do nothing because is zero
+                minlabel.Text = startingnumber.ToString();
+                wholepath = (savePath + @"\" + sequence + startingnumber);
+                //now update the path to show what we're working with
+                textBox1.Text += (wholepath);
+            }
+            else
+            {
+                // Jesus tap-dancing Christ!
+                if (Control.ModifierKeys == Keys.Shift)
+                {
+                    startingnumber = (startingnumber) - 0100;
+                    minlabel.Text = startingnumber.ToString();
+                    wholepath = (savePath + @"\" + sequence + startingnumber);
+                    //now update the path to show what we're working with
+                    textBox1.Text += (wholepath);
+                }
+                else
+                {
+                    startingnumber = (startingnumber) - 0010;
+                    minlabel.Text = startingnumber.ToString();
+                    wholepath = (savePath + @"\" + sequence + startingnumber);
+                    //now update the path to show what we're working with
+                    textBox1.Text += (wholepath);
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // raise starting number
+            if (startingnumber < 0000)
+            {
+                //do nothing because is zero
+                minlabel.Text = startingnumber.ToString();
+                wholepath = (savePath + @"\" + sequence + startingnumber);
+                //now update the path to show what we're working with
+                textBox1.Text += (wholepath);
+
+            }
+            else
+            {  
+                //Jesus tap-dancing Christ! Shift for hundreds!
+
+                if (Control.ModifierKeys == Keys.Shift)
+                {
+                    startingnumber = (startingnumber) + 0100;
+                    minlabel.Text = startingnumber.ToString();
+                    // also make sure ending number is always larger than starting
+                    if (endingnumber < startingnumber)
+                    {
+                        endingnumber = (endingnumber) + 0100;
+                        maxlabel.Text = endingnumber.ToString();
+                        wholepath = (savePath + @"\" + sequence + startingnumber);
+                        //now update the path to show what we're working with
+                        textBox1.Text += (wholepath);
+                    }
+                }
+                else
+                {
+                    startingnumber = (startingnumber) + 0010;
+                    minlabel.Text = startingnumber.ToString();
+                    // also make sure ending number is always larger than starting
+                    if (endingnumber < startingnumber)
+                    {
+                        endingnumber = (endingnumber) + 0010;
+                        maxlabel.Text = endingnumber.ToString();
+                        wholepath = (savePath + @"\" + sequence + startingnumber);
+                        //now update the path to show what we're working with
+                        textBox1.Text += (wholepath);
+                    }
+                }
+
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // lower ending number
+            if (endingnumber < 0000)
+            {
+                //do nothing because is zero
+                maxlabel.Text = endingnumber.ToString();
+            }
+            else
+            {
+                endingnumber = (endingnumber) - 0010;
+                maxlabel.Text = endingnumber.ToString();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // raise ending number
+            if (endingnumber < 0000)
+            {
+                //do nothing because is zero
+                maxlabel.Text = endingnumber.ToString();
+            }
+            else
+            {
+                endingnumber = (endingnumber) + 0010;
+                maxlabel.Text = endingnumber.ToString();
+            }
+        }
     }
 }
