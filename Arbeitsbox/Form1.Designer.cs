7d8eced35d3942f17210882dc6bec5566e1949ff
David Miller <simsaladoo@hotmail.com>
2018-11-15 20:09:28 -0800

    repurposing the tool
    migrating old stuff from launcher into this repurposed project -- adding panels for Fauna, Sounds and any other CSV creating functions we need.  Will be out actual toolbox in the future (separated from the launcher itself)

--- Arbeitsbox/Form1.Designer.cs
+++ Arbeitsbox/Form1.Designer.cs
@@ -29,290 +29,93 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MenuPanel = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.maxlabel = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.minlabel = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.OpsPanel1 = new System.Windows.Forms.Panel();
+            this.OpsPanel2 = new System.Windows.Forms.Panel();
+            this.OpsPanel3 = new System.Windows.Forms.Panel();
+            this.OpsPanel4 = new System.Windows.Forms.Panel();
+            this.MenuPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // MenuPanel
+            // 
+            this.MenuPanel.Controls.Add(this.button1);
+            this.MenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.MenuPanel.Name = "MenuPanel";
+            this.MenuPanel.Size = new System.Drawing.Size(65, 461);
+            this.MenuPanel.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
-            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(0, 76);
+            this.button1.Location = new System.Drawing.Point(0, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(359, 58);
+            this.button1.Size = new System.Drawing.Size(64, 50);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Choose Folders";
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // OpsPanel1
             // 
-            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.SystemColors.Info;
-            this.textBox1.Location = new System.Drawing.Point(1, 1);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(358, 16);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "Z:\\";
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.OpsPanel1.Location = new System.Drawing.Point(65, 0);
+            this.OpsPanel1.Name = "OpsPanel1";
+            this.OpsPanel1.Size = new System.Drawing.Size(719, 461);
+            this.OpsPanel1.TabIndex = 1;
             // 
-            // button2
+            // OpsPanel2
             // 
-            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(0, 18);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(359, 58);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Choose Directory";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.OpsPanel2.Location = new System.Drawing.Point(65, 0);
+            this.OpsPanel2.Name = "OpsPanel2";
+            this.OpsPanel2.Size = new System.Drawing.Size(719, 461);
+            this.OpsPanel2.TabIndex = 2;
             // 
-            // panel1
+            // OpsPanel3
             // 
-            this.panel1.Controls.Add(this.button6);
-            this.panel1.Controls.Add(this.button7);
-            this.panel1.Controls.Add(this.button5);
-            this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.maxlabel);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.minlabel);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(0, 18);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(359, 393);
-            this.panel1.TabIndex = 3;
+            this.OpsPanel3.Location = new System.Drawing.Point(65, 0);
+            this.OpsPanel3.Name = "OpsPanel3";
+            this.OpsPanel3.Size = new System.Drawing.Size(719, 461);
+            this.OpsPanel3.TabIndex = 3;
             // 
-            // button6
+            // OpsPanel4
             // 
-            this.button6.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.button6.FlatAppearance.BorderSize = 0;
-            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(300, 229);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(36, 44);
-            this.button6.TabIndex = 12;
-            this.button6.Text = ">";
-            this.button6.UseVisualStyleBackColor = false;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // button7
-            // 
-            this.button7.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.button7.FlatAppearance.BorderSize = 0;
-            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(208, 229);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(36, 44);
-            this.button7.TabIndex = 11;
-            this.button7.Text = "<";
-            this.button7.UseVisualStyleBackColor = false;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
-            // button5
-            // 
-            this.button5.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.button5.FlatAppearance.BorderSize = 0;
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(300, 302);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(36, 44);
-            this.button5.TabIndex = 10;
-            this.button5.Text = ">";
-            this.button5.UseVisualStyleBackColor = false;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button4
-            // 
-            this.button4.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(208, 302);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(36, 44);
-            this.button4.TabIndex = 9;
-            this.button4.Text = "<";
-            this.button4.UseVisualStyleBackColor = false;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.Color.Black;
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.ForeColor = System.Drawing.SystemColors.Menu;
-            this.textBox2.Location = new System.Drawing.Point(224, 164);
-            this.textBox2.MaxLength = 3;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(97, 22);
-            this.textBox2.TabIndex = 8;
-            this.textBox2.Text = "AA";
-            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
-            // maxlabel
-            // 
-            this.maxlabel.AutoSize = true;
-            this.maxlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.maxlabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.maxlabel.Location = new System.Drawing.Point(247, 312);
-            this.maxlabel.Name = "maxlabel";
-            this.maxlabel.Size = new System.Drawing.Size(50, 24);
-            this.maxlabel.TabIndex = 7;
-            this.maxlabel.Text = "0010";
-            this.maxlabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(23, 164);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 24);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Sequence";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(23, 240);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(146, 24);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Starting Number";
-            // 
-            // minlabel
-            // 
-            this.minlabel.AutoSize = true;
-            this.minlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.minlabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.minlabel.Location = new System.Drawing.Point(247, 240);
-            this.minlabel.Name = "minlabel";
-            this.minlabel.Size = new System.Drawing.Size(50, 24);
-            this.minlabel.TabIndex = 6;
-            this.minlabel.Text = "0000";
-            this.minlabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(23, 312);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(145, 24);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Ending Number";
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(192)))), ((int)(((byte)(75)))));
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(0, 507);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(359, 58);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "Make Folders";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.Black;
-            this.panel2.Location = new System.Drawing.Point(208, 147);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(128, 56);
-            this.panel2.TabIndex = 13;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Arbeitsbox.Properties.Resources.giphyder5p;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 417);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(358, 90);
-            this.pictureBox1.TabIndex = 14;
-            this.pictureBox1.TabStop = false;
+            this.OpsPanel4.Location = new System.Drawing.Point(65, 0);
+            this.OpsPanel4.Name = "OpsPanel4";
+            this.OpsPanel4.Size = new System.Drawing.Size(719, 461);
+            this.OpsPanel4.TabIndex = 2;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.ClientSize = new System.Drawing.Size(359, 565);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.OpsPanel4);
+            this.Controls.Add(this.OpsPanel3);
+            this.Controls.Add(this.OpsPanel2);
+            this.Controls.Add(this.OpsPanel1);
+            this.Controls.Add(this.MenuPanel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(375, 600);
-            this.MinimumSize = new System.Drawing.Size(375, 400);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 500);
+            this.MinimumSize = new System.Drawing.Size(800, 500);
             this.Name = "Form1";
             this.Text = "Arbeitsbox";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.MenuPanel.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel MenuPanel;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label maxlabel;
-        private System.Windows.Forms.Label minlabel;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel OpsPanel1;
+        private System.Windows.Forms.Panel OpsPanel2;
+        private System.Windows.Forms.Panel OpsPanel3;
+        private System.Windows.Forms.Panel OpsPanel4;
     }
 }
 
